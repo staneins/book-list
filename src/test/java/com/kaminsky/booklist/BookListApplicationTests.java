84d1217a7581d5b14bb33907af6813755e585458
Philip Kaminsky <kaminsky8836@gmail.com>
2024-12-09 12:25:26 +0300

    initial commit

---
+++ src/test/java/com/kaminsky/booklist/BookListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaminsky.booklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
